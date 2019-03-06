c1c118bd3bbbb8a772bfe1d74feb728c163f3532
Ranaghan <jae.ranaghan@uleth.ca>
2019-03-06 11:42:35 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+window.onload = init;
+
+function init() {
+	
+	// document.querySelector('a-box').addEventListener('click', function (evt) {
+ //  		console.log('This 2D element was clicked!');
+	// });
+
+	
+}
