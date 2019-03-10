eb9802fb7bfdbefbef2209f177a2311fb4b4a9d6
Brett Regnier <bretternestregnier@gmail.com>
2019-03-10 15:57:23 -0600

    clicking areas works fully now

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,11 @@ window.onload = init;
 function init() {
 	// TOOD change window inner height and window inner width to the canvas heights.
 	// TODO javascript for checking width of the window and set the canvas to that.
+	// TODO add word background to model
 
+	// replace info with real id.
+	var infos = document.getElementsByClassName("info")
+	
 	var canvas = document.getElementById("canvas");
 
 	var scene = new THREE.Scene();
@@ -22,29 +26,10 @@ function init() {
 	var mouse = new THREE.Vector2();
 	var gltfscene = null;
 	var pouch = null;
-	
+
 	// Set to something the mouse pos should never reach so make sure there are no auto selected raycasts.
 	mouse.x = 1000000;
 	mouse.y = 1000000;
-
-	function onMouseMove(event) {
-		mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-		mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-	}
-	
-	function onMouseDown(event) {
-		for (var i = 0; i < scene.children.length; i++)
-		{
-			if (scene.children[i].name == "hitbox")
-			{
-				scene.children[i].selected = false;
-				if (scene.children[i].hovering == true)
-				{
-					scene.children[i].selected = true;
-				}
-			}
-		}	
-	}
 
 	var loader = new THREE.GLTFLoader();
 	// Load a glTF resource
@@ -73,83 +58,122 @@ function init() {
 		},
 		// called while loading is progressing
 		function (xhr) {
-
 			console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
 		},
 		// called when loading has errors
 		function (error) {
-
 			console.log('An error happened');
-
 		}
 	);
 
-	// Create teh hit boxes for the hot spots on the pouch, can add stipe hitboxes for the back as well if needed
+	function onMouseMove(event) {
+		mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+		mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+	}
+
+	function onMouseDown(event) {
+		for (var i = 0; i < scene.children.length; i++)
+		{
+			var c = scene.children[i];
+			if (c.name == "hitbox")
+			{
+				c.selected = false;
+				if (c.hovering == true)
+				{
+					c.selected = true;
+					c.hovering = false;
+					
+					// This is where the modal window will be expanded upon clicking on a hot area.
+					// for (var i = 0; i < infos.children.length; i++)
+					// {
+					// 	var info = infos.children[i];
+					// 	// hide the info boxes naturally.
+					// 	// insert code here
+					// 	if (info.id = c.area)
+					// 	{
+					// 		// display
+					// 	}
+					// }
+				}
+			}
+		}
+	}
+
+	// Create the hit boxes for the hot spots on the pouch, can add stipe hitboxes for the back as well if needed
 	function ConstructHitBoxes() {
-		function CreateHitBox(w, h, d, x, y, z, rotx = 0.0, roty = 0.0, rotz = 0.0) {
+		function CreateHitBox(w, h, d, x, y, z, rotx = 0.0, roty = 0.0, rotz = 0.0, area = "") {
 			var geo = new THREE.BoxGeometry(w, h, d);
 			var mat = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
 			mat.transparent = true;
 			mat.opacity = 0.0;
-			
+
 			var cube = new THREE.Mesh(geo, mat);
 			cube.name = "hitbox";
 			cube["selected"] = false;
 			cube["hovering"] = true;
+			cube["area"] = area;
+			cube.frustumCulled = false;
 			scene.add(cube);
-			
-			
+
+
 			cube.rotation.x = rotx;
 			cube.rotation.y = roty;
 			cube.rotation.z = rotz;
-			
+
 			cube.position.x = x;
 			cube.position.y = y;
 			cube.position.z = z;
-			
+
 		}
-		
-		CreateHitBox(3.4, 3.1, 0.1, 0.11, 0.6, 1, 0.1, 0, 0.0); // front
-		CreateHitBox(3.4, 2.6, 0.1, 0.2, 0.6, -0.17, 0.2, 0.0, 0.0); // back
-		CreateHitBox(0.5, 3.0, 2.5, -1.84, 0.3, 0); // left
-		CreateHitBox(0.5, 2.5, 2.5, 2.095, 0.80, 0); // right
-		CreateHitBox(4.2, 0.4, 2.5, 0, -1.0, 0, 0.0, 0.0, 0.17); // bottom
+
+		CreateHitBox(3.5, 3.2, 0.1, 0.01, 0.5, 1.2, 0.1, 0, 0.0, "front"); // front
+		CreateHitBox(3.4, 2.6, 0.1, 0.2, 0.6, -0.17, 0.2, 0.0, 0.0, "back"); // back
+		CreateHitBox(0.5, 2.5, 2.4, -1.90, 0.1, 0.4, 0, 0, 0.21, "frills"); // left
+		CreateHitBox(0.5, 2.7, 2.4, 2.095, 0.80, 0.4, 0, 0, 0, "frills"); // right
+		CreateHitBox(3.4, 1.0, 2.4, 0.2, -1.0, 0.4, 0.0, 0.0, 0.1, "frills"); // bottom
 	}
 
 	function Render() {
 		// console.log(mouse.x);
 		// console.log(mouse.y);
-		
+
 		for (var i = 0; i < scene.children.length; i++)
 		{
-			if (scene.children[i].name == "hitbox" && scene.children[i].selected == false)
+			var c = scene.children[i];
+			c.hovering = false;
+			if (c.name == "hitbox" && c.selected == false)
 			{
-				scene.children[i].material.opacity = 0.0;
-				scene.children[i].hovering = false;
-			}	
+				c.material.opacity = 0.0;
+			}
 		}
-		
+
 		// Finds the first intersected hitbox in the scene from the mouse normal.
 		raycaster.setFromCamera(mouse, camera);
 		var intersects = raycaster.intersectObjects(scene.children);
 		if (intersects.length > 0)
 		{
+			var op = 0.15;
 			var interObj = intersects[0].object;
-			interObj.material.opacity = 0.3;
-			
-			// This needs to be placed under a click event
+			interObj.material.opacity = op;
 			interObj.hovering = true;
-			// interObj.selected = true;
+
+
+			for (var i = 0; i < scene.children.length; i++)
+			{
+				var c = scene.children[i];
+				if (c.area == interObj.area)
+				{
+					c.hovering = true;
+					c.material.opacity = op;
+				}
+			}
 		}
-		
-		// TODO add clicking events for areas that the mouse is raycasted to. 
-		// TODO add events for when the area is moused over? if possible
+
 		// TOOD add ID clicks to fill in information of the area on the HTML
-			
+
 		// TODO add rotational controls
-			
-		scene.rotation.y = -0.5;
+
+		// scene.rotation.y = -0.5;
 		// scene.rotation.y += 0.01;
 
 		renderer.render(scene, camera);
